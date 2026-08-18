7734eb0b5f34a3fc47a7aaa5449eb921e7eaabf4
aiquinones <aiquinones@users.noreply.github.com>
2026-08-18 08:21:09 +0000

    storybook: main 494186ec380ec1d71954c11de2e58ec8aa04bbb8

---
+++ assets/PromptFileRow.stories-Caz8tzTV.js
@@ -0,0 +1,48 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{E as t,b as n,g as r,h as i,l as a,n as o,r as s,v as c,z as l}from"./iframe-CqpD256r.js";import{n as u,t as d}from"./PromptFileRow-B870FvD7.js";var f,p,m,h,g,_,v,y,b,x;function S(){return(S=e((()=>{r(),u(),f=o(),p={title:`SystemPrompt/PromptFileRow`,component:d,args:{groupChars:t.chars,workspaceRoot:s,selected:!1,onSelect:()=>void 0},decorators:[e=>(0,f.jsx)(`div`,{className:`w-[520px] py-2`,children:(0,f.jsx)(e,{})})]},m={args:{file:{...l,order:1}}},h={args:{file:{...t,order:2}}},g={args:{file:{...i,order:3}}},_={args:{file:{...n,order:6}}},v={args:{file:{...c,order:4}}},y={args:{file:{...a,order:5}}},b={args:{file:t,selected:!0}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: {
+      ...userPrompt,
+      order: 1
+    }
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: {
+      ...projectPrompt,
+      order: 2
+    }
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: {
+      ...importedAgents,
+      order: 3
+    }
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: {
+      ...nestedPrompt,
+      order: 6
+    }
+  }
+}`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: {
+      ...missingImport,
+      order: 4
+    }
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: {
+      ...circularImport,
+      order: 5
+    }
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    file: projectPrompt,
+    selected: true
+  }
+}`,...b.parameters?.docs?.source}}},x=[`UserFile`,`ProjectFile`,`Imported`,`Conditional`,`BrokenImport`,`Circular`,`Selected`]})))()}S();export{v as BrokenImport,y as Circular,_ as Conditional,g as Imported,h as ProjectFile,b as Selected,m as UserFile,x as __namedExportsOrder,p as default};
---
+++ assets/iframe-DtcTYF5B.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-CqpD256r.js";e();
