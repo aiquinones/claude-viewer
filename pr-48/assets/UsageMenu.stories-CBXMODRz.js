048f2bfee0573941273afc45f4831bc4b841d7ea
aiquinones <aiquinones@users.noreply.github.com>
2026-08-19 16:06:56 +0000

    storybook: pr-48 481b7a651e5cb96e317665db6a03654029c90612

---
+++ pr-48/assets/UsageMenu.stories-CBXMODRz.js
@@ -0,0 +1,9 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{U as t,W as n,n as r}from"./iframe-Chv6712e.js";import{n as i,t as a}from"./UsageMenu-DKwoZn50.js";import{n as o,t as s}from"./SettingsContext-BxpmQuvW.js";var c,l,u,d,f,p;function m(){return(m=e((()=>{n(),o(),i(),c=r(),l=({basis:e,source:n,children:r})=>{let i={...t,usage:{...t.usage,costBasis:{value:e,source:n}}};return(0,c.jsx)(s,{settings:i,children:r})},u={title:`Usage/UsageMenu`,component:a,decorators:[e=>(0,c.jsx)(`div`,{className:`flex h-96 justify-end p-6`,children:(0,c.jsx)(e,{})})]},d={render:()=>(0,c.jsx)(l,{basis:`all`,source:`default`,children:(0,c.jsx)(a,{})})},f={render:()=>(0,c.jsx)(l,{basis:`output`,source:`user`,children:(0,c.jsx)(a,{})})},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  render: () => <WithBasis basis="all" source="default">
+      <UsageMenu />
+    </WithBasis>
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: () => <WithBasis basis="output" source="user">
+      <UsageMenu />
+    </WithBasis>
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`OutputOnly`]})))()}m();export{d as Default,f as OutputOnly,p as __namedExportsOrder,u as default};
---
+++ pr-48/assets/iframe-zXXctBNe.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Chv6712e.js";e();
