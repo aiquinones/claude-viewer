750b17552baea69ec3287841d657dea89882da3a
aiquinones <aiquinones@users.noreply.github.com>
2026-08-21 07:21:38 +0000

    storybook: main b06d513d2dc6da0d4d751e84eda2a86cf7ab8d23

---
+++ assets/PanelActions.stories-B1-2wax6.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t}from"./iframe-BtK1Z1TA.js";import{n,t as r}from"./PanelActions-05aqCrbD.js";var i,a,o,s,c;function l(){return(l=e((()=>{n(),i=t(),a={title:`Chrome/PanelActions`,component:r,args:{onSearch:()=>void 0,onRefresh:()=>void 0},decorators:[e=>(0,i.jsx)(`div`,{className:`flex justify-end p-6 pb-24`,children:(0,i.jsx)(e,{})})]},o={args:{}},s={args:{onGoToSelection:()=>void 0}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    onGoToSelection: () => undefined
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Default`,`WithGoToSelection`]})))()}l();export{o as Default,s as WithGoToSelection,c as __namedExportsOrder,a as default};
