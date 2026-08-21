943c5266f7a2096cbad1984f3f2bfe466f69f793
aiquinones <aiquinones@users.noreply.github.com>
2026-08-21 06:46:18 +0000

    storybook: pr-53 2aeaca1568aca1c01a5daea39dfb51e56c9974ba

---
+++ pr-53/assets/MemoryTypeBadge.stories-CdrJIhY8.js
@@ -0,0 +1,14 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{a as t,c as n}from"./types-BrfVTSKh.js";import{n as r}from"./iframe-D4bcnUjr.js";import{n as i,t as a}from"./MemoryTypeBadge-CYHr1vqq.js";var o,s,c,l,u,d;function f(){return(f=e((()=>{n(),i(),o=r(),s={title:`Memory/MemoryTypeBadge`,component:a},c={render:()=>(0,o.jsx)(`div`,{className:`flex gap-2 p-4`,children:t.map(e=>(0,o.jsx)(a,{type:e},e))})},l={args:{type:void 0,declaredType:`preference`}},u={args:{type:void 0}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: () => <div className="flex gap-2 p-4">
+      {MEMORY_TYPES.map(type => <MemoryTypeBadge key={type} type={type} />)}
+    </div>
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: undefined,
+    declaredType: 'preference'
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: undefined
+  }
+}`,...u.parameters?.docs?.source}}},d=[`EveryType`,`Declared`,`Untyped`]})))()}f();export{l as Declared,c as EveryType,u as Untyped,d as __namedExportsOrder,s as default};
