e3bb8cbf0263432fa20e19f375671b3d66c65fd6
aiquinones <aiquinones@users.noreply.github.com>
2026-08-22 08:23:21 +0000

    storybook: pr-68 435ae3a5e368f75f451c0363782f78f3e61cdb4b

---
+++ pr-68/assets/skill-totals-WCrAYR2t.js
@@ -0,0 +1,1 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";var t;function n(){return(n=e((()=>{t=e=>e.reduce((e,t)=>({skills:e.skills+1,chars:e.chars+t.listingChars}),{skills:0,chars:0})})))()}export{t as n,n as t};
