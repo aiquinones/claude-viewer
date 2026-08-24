b6d95c9df589cbf2ea510ee63e95b998193f114a
aiquinones <aiquinones@users.noreply.github.com>
2026-08-24 07:43:56 +0000

    storybook: main 31c54505099f35c4bf19c4a69cdd50b6df2301b9

---
+++ assets/StepColumn.stories-DbGxp7TU.js
@@ -0,0 +1,17 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{N as t,R as n,g as r,n as i}from"./iframe-DZrjPOII.js";import{i as a,r as o}from"./surfaces-DW11G-_u.js";import{n as s,t as c}from"./StepColumn-D5pIe_Qg.js";var l,u,d,f,p,m,h;function g(){return(g=e((()=>{r(),o(),s(),l=i(),u={title:`Skills/Flow/StepColumn`,component:c,args:{steps:n.steps,focusedStepId:void 0,compact:!1},decorators:[e=>(0,l.jsx)(`div`,{className:`w-[30rem] p-6`,style:{"--surface-accent":a(`skills`)},children:(0,l.jsx)(e,{})})]},d={},f={args:{focusedStepId:n.steps[2].id}},p={args:{focusedStepId:n.steps[2].id,compact:!0},decorators:[e=>(0,l.jsx)(`div`,{className:`w-44 p-2`,children:(0,l.jsx)(e,{})})]},m={args:{steps:t.steps}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    focusedStepId: stepFlow.steps[2].id
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    focusedStepId: stepFlow.steps[2].id,
+    compact: true
+  },
+  decorators: [Story => <div className="w-44 p-2">
+        <Story />
+      </div>]
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    steps: sectionFlow.steps
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Default`,`Focused`,`Rail`,`UnnumberedSections`]})))()}g();export{d as Default,f as Focused,p as Rail,m as UnnumberedSections,h as __namedExportsOrder,u as default};
