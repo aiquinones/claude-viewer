7790e115b749fcb36a5e88c28d429e9ed53d24d7
aiquinones <aiquinones@users.noreply.github.com>
2026-08-25 16:07:59 +0000

    storybook: pr-85 ee4ddd3f0f9affecd88c983576db809ff2b96c14

---
+++ pr-85/assets/PanelMenu.stories-Ef_3fmMn.js
@@ -0,0 +1,13 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{at as t,it as n,n as r}from"./iframe-B-u7NCS7.js";import{n as i,t as a}from"./SettingsContext-XWY9TKld.js";import{n as o,t as s}from"./PanelMenu-sS4uywAI.js";var c,l,u,d,f,p,m;function h(){return(h=e((()=>{t(),o(),i(),c=r(),l=({mode:e,children:t})=>{let r={...n,theme:{mode:{value:e,source:`user`}}};return(0,c.jsx)(a,{settings:r,children:t})},u={title:`Chrome/PanelMenu`,component:s,decorators:[e=>(0,c.jsx)(`div`,{className:`flex h-[24rem] justify-end p-6`,children:(0,c.jsx)(e,{})})]},d={render:()=>(0,c.jsx)(l,{mode:`auto`,children:(0,c.jsx)(s,{})})},f={render:()=>(0,c.jsx)(l,{mode:`dark`,children:(0,c.jsx)(s,{})})},p={render:()=>(0,c.jsx)(l,{mode:`inherit`,children:(0,c.jsx)(s,{})})},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  render: () => <WithTheme mode="auto">
+      <PanelMenu />
+    </WithTheme>
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: () => <WithTheme mode="dark">
+      <PanelMenu />
+    </WithTheme>
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: () => <WithTheme mode="inherit">
+      <PanelMenu />
+    </WithTheme>
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`PanelDark`,`InheritsEveryColor`]})))()}h();export{d as Default,p as InheritsEveryColor,f as PanelDark,m as __namedExportsOrder,u as default};
