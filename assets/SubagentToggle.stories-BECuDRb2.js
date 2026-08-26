656207cf7c57311757bc9d3fdf4c2f48b4963598
aiquinones <aiquinones@users.noreply.github.com>
2026-08-26 21:06:31 +0000

    storybook: main 4ad9d887ef943f22504a6f8457d7130286fba830

---
+++ assets/SubagentToggle.stories-BECuDRb2.js
@@ -0,0 +1,16 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t}from"./iframe-Cq9tp2r5.js";import{n,t as r}from"./SubagentToggle-CKcozleU.js";var i,a,o,s,c,l;function u(){return(u=e((()=>{n(),i=t(),a={title:`Agents/SubagentToggle`,component:r,args:{onToggle:()=>void 0},decorators:[e=>(0,i.jsx)(`div`,{className:`w-[420px] p-6`,children:(0,i.jsx)(e,{})})]},o={args:{count:3,open:!1}},s={args:{count:3,open:!0}},c={args:{count:1,open:!1}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 3,
+    open: false
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 3,
+    open: true
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 1,
+    open: false
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Collapsed`,`Expanded`,`One`]})))()}u();export{o as Collapsed,s as Expanded,c as One,l as __namedExportsOrder,a as default};
